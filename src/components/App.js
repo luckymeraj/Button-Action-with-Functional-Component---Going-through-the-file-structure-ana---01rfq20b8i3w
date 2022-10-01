f2df5223db47708e7cbf0fe86fd4fb32b8663b0a
Meraj Rizwan <107464610+luckymeraj@users.noreply.github.com>
2022-10-01 17:30:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,19 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[tPara,setTPara]=useState("")
+  const txt="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+  const onClickHandler=()=>{
+    setTPara(txt)
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+
+    <button id="click" onClick={onClickHandler} >clickme</button>
+    <p id="para">{tPara}</p>
+    
     </div>
   );
 }
-
 
 export default App;
