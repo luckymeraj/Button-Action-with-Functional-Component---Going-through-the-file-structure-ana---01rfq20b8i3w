612dbe7441d858fffdeeb7a631fd5d6ecfd299b5
Meraj Rizwan <107464610+luckymeraj@users.noreply.github.com>
2022-10-13 19:47:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const[tPara,setTPara]=useState()
+  const[tPara,setTPara]=useState(false)
   const txt="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
   const onClickHandler=()=>{
-    setTPara(txt)
+    setTPara(true)
   }
   return (
     <div id="main">
 
-    <button id="click" onClick={onClickHandler} >clickme</button>
-    <p id="para">{tPara}</p>
+    <button id="click" onClick={onClickHandler} >click</button>
+     {tPara && (
+        <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      )}
     
     </div>
   );
