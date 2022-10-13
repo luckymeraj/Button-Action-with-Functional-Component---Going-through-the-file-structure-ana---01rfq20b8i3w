6add78ce046a58183fbdcfef617e57b413b69897
Meraj Rizwan <107464610+luckymeraj@users.noreply.github.com>
2022-10-13 19:21:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const[tPara,setTPara]=useState("")
+  const[tPara,setTPara]=useState()
   const txt="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
   const onClickHandler=()=>{
     setTPara(txt)
